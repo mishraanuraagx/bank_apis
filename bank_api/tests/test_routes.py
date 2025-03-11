6514dfe6fe2a7dbc81881b787ca1dbd8f0a80791
Anuraag Mishra <mishraanuraagx@gmail.com>
2025-03-11 20:28:33 +0100

    removed unnecessary working directories for pytest console run requirement.

--- bank_api/tests/test_routes.py
+++ bank_api/tests/test_routes.py
@@ -1,10 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-if os.path.basename(os.getcwd()) == "tests":
-    os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), 'bank_api/app')))
 
 import pytest
 from fastapi.testclient import TestClient
